2b2793a630a31f24ff92ce6943d9eab62d0c10a0
Abhijeet72i <104633485+Abhijeet72i@users.noreply.github.com>
2024-09-08 23:29:22 +0530

    [383] [Time Beats: 42.69%] [Memory Beats: 33.33%] - LeetPush

---
+++ 383-Ransom-Note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> umap;
+        for(int i =0;i<magazine.size();i++){
+            umap[magazine[i]]++;
+         
+        }
+        for(int i=0;i<ransomNote.size();i++){
+            if(umap[ransomNote[i]]>0){
+            umap[ransomNote[i]]--;
+            }
+            else{
+                return false;
+            }
+        }
+return true;
+    }
+};
